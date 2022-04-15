bc7cfad80b6ba1a08e49b60426e201ce60687ee8
Phil Wang <lucidrains@gmail.com>
2022-04-15 13:37:57 -0700

    prepare ability to pass boundary sequence indices to indicate where autoregressive masking starts, for GLM type training to be possible

--- protein_glm/protein_glm.py
+++ protein_glm/protein_glm.py
@@ -21,6 +21,14 @@ class PreNormResidual(nn.Module):
     def forward(self, x, **kwargs):
         return self.fn(self.norm(x), **kwargs)
 
+def FeedForward(dim, mult = 4):
+    inner_dim = int(mult * dim)
+    return nn.Sequential(
+        nn.Linear(dim, inner_dim),
+        nn.GELU(),
+        nn.Linear(inner_dim, dim)
+    )
+
 class Attention(nn.Module):
     def __init__(
         self,
@@ -36,7 +44,11 @@ class Attention(nn.Module):
         self.to_kv = nn.Linear(dim, dim_head * 2, bias = False)
         self.to_out = nn.Linear(inner_dim, dim, bias = False)
 
-    def forward(self, x):
+    def forward(
+        self,
+        x,
+        causal_boundary_indices = None
+    ):
         """
         einstein notation:
         b - batch
@@ -44,12 +56,24 @@ class Attention(nn.Module):
         d - feature dimension
         n, i, j - sequence (main, source, target)
         """
+        n, device = x.shape[1], x.device
 
         q = self.to_q(x)
         k, v = self.to_kv(x).chunk(2, dim = -1)
 
         q = q * self.scale
         sim = einsum('b h i d, b j d -> b h i j', q, k)
+
+        i, j = sim.shape[-2:]
+        max_neg_value = -torch.finfo(sim.dtype).max
+        causal_mask = torch.ones((i, j), dtype = torch.bool, device = device).triu(j - i + 1)
+
+        if exists(causal_boundary_indices):
+            seq_range = torch.arange(n, device = device)
+            causal_mask_after = rearrange(seq_range, 'j -> 1 1 1 j') >= rearrange(causal_boundary_indices, 'b -> b 1 1 1')
+            causal_mask = causal_mask & causal_mask_after
+
+        sim = sim.masked_fill(causal_mask, max_neg_value)
 
         attn = sim.softmax(dim = -1)
 
@@ -65,17 +89,31 @@ class ProteinGLM(nn.Module):
         *,
         dim,
         depth,
+        num_tokens,
         dim_head = 64,
-        heads = 8
+        heads = 8,
+        ff_mult = 4,
     ):
         super().__init__()
 
         self.layers = nn.ModuleList([])
         for _ in range(depth):
-            self.layers.append(PreNormResidual(dim, Attention(dim = dim, dim_head = dim_head, heads = heads)))
+            self.layers.append(nn.ModuleList([
+                PreNormResidual(dim, Attention(dim = dim, dim_head = dim_head, heads = heads)),
+                PreNormResidual(dim, FeedForward(dim = dim, mult = ff_mult)),
+            ]))
 
-    def forward(self, x):
-        for attn in self.layers:
-            x = attn(x)
+        self.norm = nn.LayerNorm(dim)
+        self.to_logits = nn.Linear(dim, num_tokens, bias = False)
 
-        return x
+    def forward(
+        self,
+        x,
+        causal_boundary_indices = None # the sequence indices, per batch, at which autoregressive mask starts (before the index would be full bidirectional attention)
+    ):
+        for attn, ff in self.layers:
+            x = attn(x, causal_boundary_indices = causal_boundary_indices)
+            x = ff(x)
+
+        x = self.norm(x)
+        return self.to_logits(x)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'protein-glm',
   packages = find_packages(exclude=[]),
-  version = '0.0.1',
+  version = '0.0.2',
   license='MIT',
   description = 'Protein generative model with General Language Model PreTraining (GLM)',
   author = 'Phil Wang',
